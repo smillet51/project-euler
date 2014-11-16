3a1005de48a0883853c632b17220f2331bdc7017
Stephen Millet <smillet15@gmail.com>
2014-11-16 15:20:46 -0500

    Add tests and a bug fix found by tests.
    Since small numbers might have a sqrt == 2 add +1 to the range used for testing.

--- primes.py
+++ primes.py
@@ -13,7 +13,9 @@ def is_prime(num):
     sqrt = int(math.sqrt(num))
 
     # use xrange to generate the list as we iterate
-    for i in xrange(2, sqrt):
+    # use sqrt + 1 to handle smaller composites where the sqrt is 2.
+    # we need an initial case to test fail out as False.
+    for i in xrange(2, sqrt + 1):
         if num % i == 0:
             return False
 
@@ -32,6 +34,22 @@ def generate_list_of_primes(min, max):
     return primes
 
 if __name__ == "__main__":
-    print generate_list_of_primes(1, 100)
-    print generate_list_of_primes(1000, 9999)
-        
+    # check for some simple prime and not prime numbers
+    assert not is_prime(1), "1 should not be prime"
+    assert is_prime(5), "5 should be prime"
+    assert not is_prime(6), "6 should not be prime"
+    assert not is_prime(100), "100 should be be prime"
+    assert not is_prime(1000), "1000 should not be prime"
+
+    # check the primes from the problem 49 example
+    assert is_prime(1487), "1487 should be prime"
+    assert is_prime(4817), "4817 should be prime"
+    assert is_prime(8147), "8147 should be prime"
+
+    for num in generate_list_of_primes(1, 100):
+        assert is_prime(num), "%s should be prime" % num
+
+    for num in generate_list_of_primes(1000, 9999):
+        assert is_prime(num), "%s should be prime" % num
+
+    print "all assertions passed"
