9bd495523a910e7258621b794804615e809c8c5c
Stephen Millet <smillet15@gmail.com>
2014-11-16 15:53:56 -0500

    Add reversible related tools and tests.

---
+++ reversible.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+
+""" Some tools for dealing with reversible numbers for problem 145 from Project Euler.
+    https://projecteuler.net/problem=145
+"""
+
+
+def is_odd(num):
+    """ Check if an integer is odd. """
+    if num % 2 != 0:
+        return True
+    else:
+        return False
+
+
+def is_reversible(num):
+    """ Check if a number is reversible given the above definition. """
+    num_str = str(num)
+    rev_num = int("".join(reversed(num_str)))
+
+    total = num + rev_num
+
+    for digit in str(total):
+        if not is_odd(int(digit)):
+            return False
+
+    return True
+
+
+if __name__ == "__main__":
+    # check some odd and even numbers
+    assert is_odd(1), "1 should be odd"
+    assert not is_odd(2), "2 should not be odd"
+    assert not is_odd(100), "100 should not be odd"
+    assert is_odd(10001), "10001 should be odd"
+
+    # check the example reversible numbers
+    assert is_reversible(36), "36 should be reversible"
+    assert is_reversible(63), "63 should be reversible"
+    assert is_reversible(409), "409 should be reversible"
+    assert is_reversible(904), "904 should be reversible"
+        
+    print "all assertions passed"
